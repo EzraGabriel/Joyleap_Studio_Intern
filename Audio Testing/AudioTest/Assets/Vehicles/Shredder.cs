3b8a3d5411077d51ca6119d67ece1c34b8cdb616
EzraGabriel <ezragabrielm@gmail.com>
2024-02-03 13:48:13 +0700

    Folder AudioTesting

---
+++ Audio Testing/AudioTest/Assets/Vehicles/Shredder.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shredder : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Car")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
